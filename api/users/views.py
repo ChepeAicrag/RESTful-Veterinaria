49712ae9cf322fea9da26c7791535ac9337e88ef
ChepeAicrag <chepeaicrag12@gmail.com>
2022-06-15 08:33:14 -0500

    Updated: views of user

--- api/users/views.py
+++ api/users/views.py
@@ -1,6 +1,5 @@
-from django.urls import reverse
 from django.conf import settings
-
+from django.db import transaction
 from django.forms import ValidationError
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,48 +8,42 @@ from rest_framework.permissions import AllowAny
 
 import jwt
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth import authenticate
 
-from .models import User
-from .utils import Util
-from .serializers import UserSignupSerializer, UserLoginSerializer
-
-
-# SIGNUP
-"""
-    User singup.  Input example:
-    {
-        "name":"NameExample",
-        "email":"email@email.com",
-        "password": "mysuperpassword"
-    }
-"""
+from .models import Role, User
+from .serializers import AddressSerializer, UserSignupSerializer, UserLoginSerializer
 
 
 class UserSignupView(APIView):
     permission_classes = (AllowAny, )
 
     def post(self, request):
-        data = request.data
-        serializer = UserSignupSerializer(data=request.data)
+        data = request.data.copy()
+        serializer = AddressSerializer(
+            data=data.get('address', None))
         serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        user_data = serializer.data
-        user = User.objects.get(email=user_data['email'])
-        token = RefreshToken.for_user(user).access_token
-        current_site = get_current_site(request).domain
-        relativeLink = reverse('email-verify')
-
-        absurl = 'http://' + current_site + \
-            relativeLink + "?token=" + str(token)
-        email_body = 'Gracias por registrarte a Get Talent, por favor verifica tu correo electrónico en la siguiente liga:\n' + absurl
-        data = {'email_body': email_body, 'to_email': user.email,
-                'email_subject': 'Get Talent, Confirma tu correo electrónico'}
-        Util.send_email(data)
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        user = data.get('user', None)
+        if user is None:
+            return Response({'message': 'La información del usuario es requerida'}, status=status.HTTP_400_BAD_REQUEST)
+        role = Role.objects.filter(id=user['role'])
+        if not role:
+            return Response({'message': 'El rol dado no existe'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            with transaction.atomic():
+                serializer.save()
+                user['address'] = serializer.data['id']
+                serializer = UserSignupSerializer(data=user)
+                if not serializer.is_valid():
+                    raise ValidationError(serializer.errors)
+                serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            transaction.rollback()
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            transaction.rollback()
+            print(error)
+            return Response({'message': 'Error al registrar usuario'}, status=status.HTTP_400_BAD_REQUEST)
 
 # VERIFICAR EMAIL
 
@@ -130,33 +123,13 @@ class UserLoginView(APIView):
         # Valida si el correo existe
         user_email = User.objects.filter(email=email)
         if not user_email.exists():
-            raise ValidationError({
+            return Response({
                 'msg': 'No existe una cuenta con este correo registrado'
-            })
+            }, status=status.HTTP_400_BAD_REQUEST)
 
         # Valida que el email este verificado
         user_active = User.objects.get(email=email)
         if user_active == False:
-            raise ValidationError({
+            return Response({
                 'msg': 'La cuenta no ha está activada, por favor verifica tu email'
-            })
-
-        # Valida que la contraseña sea correcta
-        user = User.objects.get(email=email)
-        while not User.objects.filter(password=password):
-            while user.login_attempts < 3:
-                user.login_attempts += 1
-                user.save()
-                if user.login_attempts == 1:
-                    raise ValidationError({
-                        'msg': 'Contraseña incorrecta, trata de nuevo'
-                    })
-                elif user.login_attempts == 2:
-                    raise ValidationError({
-                        'msg': 'Contraseña incorrecta, si acumulas tres fallas puedes bloquear tu cuenta'
-                    })
-            else:
-                user.is_active = False
-                user.save()
-                raise ValidationError(
-                    {'msg': 'Por seguridad, la cuenta ha sido bloqueada'})
+            }, status=status.HTTP_400_BAD_REQUEST)
