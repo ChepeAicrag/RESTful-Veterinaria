1c1a2f2076464733fd7025b19b5e27df69d23cc4
José Ángel García <chepeaicrag12@gmail.com>
2022-06-16 12:53:48 -0500

    Updated: added urls type pet

--- api/pets/urls.py
+++ api/pets/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import ListCreatePet, ListUpdateDeletePet, ListCreateBreed, ListUpdateDeleteBreed
+from .views import ListCreatePet, ListUpdateDeletePet, ListCreateBreed, ListUpdateDeleteBreed, ListCreateTypePet, ListUpdateDeleteTypePet
 
 urlpatterns = [
     path('', ListCreatePet.as_view()),
     path('<int:id>/', ListUpdateDeletePet.as_view()),
     path('breed/', ListCreateBreed.as_view()),
     path('breed/<int:id>/', ListUpdateDeleteBreed.as_view()),
+    path('type_pet/', ListCreateTypePet.as_view()),
+    path('type_pet/<int:id>/', ListUpdateDeleteTypePet.as_view()),
 ]
