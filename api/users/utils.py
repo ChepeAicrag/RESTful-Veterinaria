ad56deab6e087f526b95e7d52b37c180fd419ca7
José Ángel García <chepeaicrag12@gmail.com>
2022-06-18 23:54:28 -0500

    Updated: utilÃs users

--- api/users/utils.py
+++ api/users/utils.py
@@ -1,10 +1,20 @@
-from django.core.mail import EmailMessage
+from django.conf import settings
+from django.core.mail import get_connection, EmailMultiAlternatives, BadHeaderError
+from django.http import HttpResponse
+
+from django.template.loader import get_template
 
 
-class Util:
-    @staticmethod
-    def send_email(data):
-        print(data)
-        email = EmailMessage(
-            subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
-        email.send()
+def send_email_validation(subject, email, message):
+    try:
+        mail = EmailMultiAlternatives(
+            subject,
+            'HUX_GYM',
+            settings.EMAIL_HOST_USER,
+            [email],
+        )
+        mail.attach_alternative(message, 'text/html')
+        mail.send(fail_silently=True)
+    except BadHeaderError:
+        return HttpResponse('Error al enviar el correo')
+    return True
