e77b9a2acd184e9c4aeab4ca50e232f9ac031c52
José Ángel García <chepeaicrag12@gmail.com>
2022-06-18 23:54:42 -0500

    Updated: views users

--- api/users/views.py
+++ api/users/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import transaction
 from django.forms import ValidationError
+from django.shortcuts import redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -72,9 +73,7 @@ class VerifyEmail(APIView):
             if not user.is_active:
                 user.is_active = True
                 user.save()
-
             return Response({'message': 'Cuenta activada exitosamente'}, status=status.HTTP_200_OK)
-
         except jwt.ExpiredSignatureError as identifier:
             return Response({'message': 'La liga de activación expiró'}, status=status.HTTP_400_BAD_REQUEST)
 
