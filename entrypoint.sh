9cf7f143b80485946ee892b3df33ddea23d0de49
José Ángel García <chepeaicrag12@gmail.com>
2022-06-22 20:34:15 -0500

    Updated: entrypoint for production

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ python manage.py migrate
 
 # Apply site migrations
 echo "Apply site migrations"
-sed -i "s,newDomain,$DOMAIN," data/data.json
-python manage.py loaddata data/data.json
+python manage.py loaddata users/fixtures/role.json
 
 exec "$@"
