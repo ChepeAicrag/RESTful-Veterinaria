b2a585dcc70d24d8f54a91be4bf074e1d862ed1e
José Ángel García <chepeaicrag12@gmail.com>
2022-06-23 02:23:33 -0500

    Updated: added urls of app appointment

--- api/core/urls.py
+++ api/core/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('pets/', include('pets.urls')),
+    path('appointments/', include('appointments.urls')),
 ]
